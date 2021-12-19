619c36094afce32197072affb5567354cb396df0
NeskoDorc <nebojsa85.milosevic@gmail.comm>
2021-12-19 12:39:49 +0100

    First_Design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="header">
+        <div className="left-header">jojojoj</div>
+        <div className="logo">logo</div>
+        <div className="right-header">
+          <div className="header-search">
+            <input type="text" placeholder='serch' />
+          </div>
+        <div className="header-profile">
+          <h3>N</h3>
+        </div>
+
+
+        </div>
+
+
+      </div>
+
+
+      <div className="base">
+      <div className="board-lists">
+                <div className="board-list">
+                    <div className="list-title">
+                        List title
+                    </div>
+                    <div className="card">
+                        <div className="card-title">card test</div>
+                        <div className="card-edit"><span><i className="fas fa-pen"></i></span></div>
+                    </div>
+
+                </div>
+               
+                
+                
+             
+                
+
+
+            </div>
+            </div>
     </div>
   );
 }
